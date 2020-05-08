cfc3c75fe7177126bb6603280a5995c502ea6d33
David Padilla <dpadman2@gmail.com>
2020-05-08 13:51:14 -0500

    express cars&cats

---
+++ server.js
@@ -0,0 +1,36 @@
+// imports
+
+const express = require("express");
+const ejs = require('ejs');
+
+// settings
+const app = express();
+app.use(express.static(__dirname + '/static'));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+// routing
+
+// app.get('/', (req, res) => {
+//     res.send("Hello Express");
+// });
+
+app.get("/users", (req, res) => {
+    // hard-coded user data
+    var users_array = [
+        {name: "Michael", email: "michael@codingdojo.com"}, 
+        {name: "Jay", email: "jay@codingdojo.com"}, 
+        {name: "Brendan", email: "brendan@codingdojo.com"}, 
+        {name: "Andrew", email: "andrew@codingdojo.com"}
+    ];
+    res.render('users.ejs', {users: users_array});
+})
+
+
+
+
+
+
+
+
+app.listen(8000, () => console.log("listening on port 8000"));
